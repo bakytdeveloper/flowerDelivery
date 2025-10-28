61f1beefa19b8d08a50bdf22ab4c574386863b4c
bakytdeveloper <bakytdeveloper@gmail.com>
2025-10-29 00:14:31 +0600

    Поменял карту, чтобы на ней можно было видеть границу

--- client/src/components/MapSection/MapSection.js
+++ client/src/components/MapSection/MapSection.js
@@ -14,8 +14,7 @@ const MapSection = () => {
 
                 <div className="map-container">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1160.639192737201!2d74.71230414741754!3d43.026295473687895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389ebdbc4eec442b%3A0x6c5bce77e072ac98!2z0JrQvtGA0LTQsNC5LCDQmtCw0LfQsNGF0YHRgtCw0L0!5e1!3m2!1sru!2skg!4v1761662341485!5m2!1sru!2skg"
-                        width="100%"
+                        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d1733.8264014302763!2d74.70850274009078!3d43.02807312173589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z0JrQvtGA0LTQsNC5INGD0Lsu0JbQmNCR0JXQmiDQltCe0JvQqyAyMzQ!5e1!3m2!1sru!2skg!4v1761675010196!5m2!1sru!2skg"                        width="100%"
                         height="450"
                         style={{border: 0}}
                         allowFullScreen=""
