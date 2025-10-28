c0d08023179a56da9d0887ca167cbf2ac38ce6bc
bakytdeveloper <bakytdeveloper@gmail.com>
2025-10-28 20:18:18 +0600

    Заменил валюту, вместо RUB сделал KZT

--- client/src/components/HitsSection/HitsSection.js
+++ client/src/components/HitsSection/HitsSection.js
@@ -91,7 +91,7 @@ const HitsSection = () => {
     const formatPrice = (price) => {
         return new Intl.NumberFormat('ru-RU', {
             style: 'currency',
-            currency: 'RUB',
+            currency: 'KZT',
             minimumFractionDigits: 0
         }).format(price);
     };
--- client/src/components/SeasonalOffers/SeasonalOffers.js
+++ client/src/components/SeasonalOffers/SeasonalOffers.js
@@ -91,7 +91,7 @@ const SeasonalOffers = () => {
     const formatPrice = (price) => {
         return new Intl.NumberFormat('ru-RU', {
             style: 'currency',
-            currency: 'RUB',
+            currency: 'KZT',
             minimumFractionDigits: 0
         }).format(price);
     };
