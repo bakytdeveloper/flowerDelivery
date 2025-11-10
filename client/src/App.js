2897eebdd05b891eab994791fca730acdad0f279
bakytdeveloper <bakytdeveloper@gmail.com>
2025-11-10 21:35:19 +0600

    Скорректировал App.js фаил

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import LoadingSpinner from "./LoadingSpinner";
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from "./contexts/CartContext";
-import { AppProvider } from './contexts/AppContext'; // ДОБАВЛЕН
+import { AppProvider } from './contexts/AppContext'; 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
